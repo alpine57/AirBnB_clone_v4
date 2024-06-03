53e30a1893f7577279e464f2302eeeb41ee44261
alpine57 <kamutileeon@gmail.com>
2024-06-03 12:31:42 -0700

    dynamics

---
+++ web_dynamic/1-hbnb.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a script that starts a flask application"""
+from flask import Flask, render_template
+import uuid
+
+app = Flask(__name__)
+
+
+@app.route('/1-hbnb/')
+def display_hbnb():
+    cache_id = uuid.uuid4()
+    try:
+        return render_template('1-hbnb.html', cache_id=cache_id)
+    except TemplateNotFound:
+        return render_template('8-hbnb.html', cache_id=cache_id)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
